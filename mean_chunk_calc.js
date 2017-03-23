a6ef149c3d1ffe2489b93b3802c1472fb641de63
Samule1 <ham_car@hotmail.com>
2017-03-23 15:21:13 +0100

    fixed

--- mean_chunk_calc.js
+++ mean_chunk_calc.js
@@ -4,35 +4,20 @@
 module.exports = {
     mean: function (db, run){
         db.get_log_data(run,(log) =>{
-            //console.log(JSON.stringify(log,null,2))
+            
+            
             let logs_with_chunks = log.filter((l) => {return l.chunks.length > 0});
-            let a = [];
-
-            logs_with_chunks.forEach((log) => {a.push(log.chunks)});
+            let a = logs_with_chunks.reduce((prev, current) =>{
+                return prev.concat(current.chunks)
+            }, []);
+            
             let total_time = a.reduce((total,chunk)=>{
-                console.log("TOTAL: "+ total + "Chunk: "+ JSON.stringify(chunk))
                 return total + (chunk.stopChunkProcessing - chunk.startChunkProcessing)
             },0)
-            /*
-            let chunks = logs_with_chunks.reduce((tot, log)=>{
-                console.log("The current chunk"+ log.chunks)
-            },[]);
 
-            */
-
-           console.log(total_time)
-           //console.log(JSON.stringify(a,null,2))
+           console.log((total_time/a.length)/1000)
+           
         })
     }
 }
 
-
-/*
-function getlogdb(){
-    console.log(run);
-    logDb.get_log_data(run, function (docs) {
-        console.log(JSON.stringify(docs, null, 2));
-    });
-}
-
-*/
